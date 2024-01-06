2e7fde6fb3bde549fc617064f0158205404bdf5e
umanghome <umanghome@gmail.com>
2024-01-06 13:14:28 +0000

    Deploying to gh-pages from @ umanghome/kia@6b880f2cfe207acd3880d0d827f7eca28e058830 🚀

---
+++ assets/help-support-b84e754b.js
@@ -0,0 +1,20 @@
+import{n as s,r as t,k as a,L as r}from"./index-da42a11f.js";import{i,S as m}from"./i18n-e674aff7.js";import{M as l}from"./index.modern-cb725893.js";const n={en:`
+# Help and Support
+## Immediate help
+For information on bus status, next available bus, timings, and issues with current bus please reach out to the BMTC Vayu Vajra helpline at [77609 91269](tel:7760991269)
+## Community help
+Join the Friends of BMTC [Telegram group](bit.ly/friendsofbmtc) for any queries, and doubts that other active members of the community can help with.
+## Technical help
+The site is designed and developed by Team Bengawalk. In case of bugs, issues or feature requests for the application, reach out to us on [Discord](https://discord.gg/XhmvDP4kXp) or [Github](https://github.com/bengawalk/kia).  
+You can also find us on [Twitter](https://twitter.com/bengawalk), [Instagram](https://www.instagram.com/bengawalk/), and [YouTube](https://www.youtube.com/@bengawalk).  
+`,kn:`
+# ಸಹಾಯ ಮತ್ತು ಬೆಂಬಲ
+## ತಕ್ಷಣದ ಸಹಾಯ
+ಬಸ್ ಸ್ಥಿತಿ, ಮುಂದಿನ ಲಭ್ಯವಿರುವ ಬಸ್, ಸಮಯ ಮತ್ತು ಪ್ರಸ್ತುತ ಬಸ್‌ನ ಸಮಸ್ಯೆಗಳ ಕುರಿತು ಮಾಹಿತಿಗಾಗಿ ದಯವಿಟ್ಟು BMTC ವಾಯು ವಜ್ರ ಸಹಾಯವಾಣಿಯನ್ನು [77609 91269](ದೂರವಾಣಿ:7760991269) ನಲ್ಲಿ ಸಂಪರ್ಕಿಸಿ
+## ಸಮುದಾಯ ಸಹಾಯ
+ಸಮುದಾಯದ ಇತರ ಸಕ್ರಿಯ ಸದಸ್ಯರು ಸಹಾಯ ಮಾಡಬಹುದಾದ ಯಾವುದೇ ಪ್ರಶ್ನೆಗಳು ಮತ್ತು ಅನುಮಾನಗಳಿಗಾಗಿ BMTC [ಟೆಲಿಗ್ರಾಮ್ ಗುಂಪು](bit.ly/friendsofbmtc) ನ ಸ್ನೇಹಿತರನ್ನು ಸೇರಿ.
+## ತಾಂತ್ರಿಕ ಸಹಾಯ
+ಸೈಟ್ ಅನ್ನು ಟೀಮ್ ಬೆಂಗಾವಾಕ್ ವಿನ್ಯಾಸಗೊಳಿಸಿದೆ ಮತ್ತು ಅಭಿವೃದ್ಧಿಪಡಿಸಿದೆ. ಅಪ್ಲಿಕೇಶನ್‌ಗಾಗಿ ದೋಷಗಳು, ಸಮಸ್ಯೆಗಳು ಅಥವಾ ವೈಶಿಷ್ಟ್ಯದ ವಿನಂತಿಗಳ ಸಂದರ್ಭದಲ್ಲಿ, [ಡಿಸ್ಕಾರ್ಡ್](https://discord.gg/XhmvDP4kXp) ಅಥವಾ [ಗಿಥಬ್‌ನಲ್ಲಿ](https://github.com/bengawalk/kia) ನಮ್ಮನ್ನು ಸಂಪರ್ಕಿಸಿ.  
+ನೀವು ನಮ್ಮನ್ನು [ಟ್ವಿಟರ್](https://twitter.com/bengawalk), [ಇನ್ಸ್ಟಾಗ್ರಾಮ್](https://www.instagram.com/bengawalk/) ಮತ್ತು [ಯೂಟ್ಯೂಬ್](https://www.youtube.com/@bengawalk) ನಲ್ಲಿಯೂ ಕಾಣಬಹುದು.
+`},u=()=>{const[e,o]=t.useState(a.getItem("lang")||r[0].code);return t.useEffect(()=>{a.setItem("lang",e),document.documentElement.setAttribute("lang",e),i.changeLanguage(e)},[e]),t.createElement(t.Fragment,null,t.createElement(m,{setLang:o}),t.createElement(l,null,n[e]||n.en))},c=s(document.getElementById("root"));c.render(t.createElement(u,null));
+//# sourceMappingURL=help-support-b84e754b.js.map
---
+++ assets/install-f4e66a5d.js
@@ -0,0 +1,54 @@
+import{n as p,r as e,k as s,L as m}from"./index-da42a11f.js";import{i as g,S as h}from"./i18n-e674aff7.js";import{M as w}from"./index.modern-cb725893.js";const a="/assets/install_android_1-1df71918.webp",n="/assets/install_android_2-db96f1b0.webp",i="/assets/install_ios_1-f83f7c7f.webp",o="/assets/install_ios_2-96a7ec7f.webp",r="/assets/install_ios_3-f47ff182.webp",l="/assets/install_desktop-e368c155.webp",c={en:`
+# Install app
+The application is a [Progressive Web App(PWA)](https://en.wikipedia.org/wiki/Progressive_web_app) and hence, can be installed and used on any mobile/desktop device.  
+On installation, it behaves like a native app accessible from the Home screen launcher. This gives a faster, and more convenient experience while always staying up-to-date.
+
+## Installation instructions
+### Android
+- Click on the menu icon beside the address bar
+- Select "Install App" or "Add to Home screen" and confirm
+
+<img src="${a}" alt="" width="250px" />
+<img src="${n}" alt="" width="250px" />
+
+### iOS
+- Open the site in Safari
+- Click the share icon at the bottom
+- Click on "Add to Home Screen"
+
+<img src="${i}" alt="" width="250px" />
+<img src="${o}" alt="" width="250px" />
+<img src="${r}" alt="" width="250px" />
+
+### Chrome
+- Click the install icon on the address bar and confirm
+
+<img src="${l}" alt="" width="750px" />
+`,kn:`
+# ಅಪ್ಲಿಕೇಶನ್ ಇನ್ಸ್ಟಾಲ್ ಮಾಡಿ
+ಅಪ್ಲಿಕೇಶನ್ ಒಂದು [ಪ್ರಗತಿಶೀಲ ವೆಬ್ ಅಪ್ಲಿಕೇಶನ್(PWA)](https://en.wikipedia.org/wiki/Progressive_web_app) ಮತ್ತು ಆದ್ದರಿಂದ, ಯಾವುದೇ ಮೊಬೈಲ್/ಡೆಸ್ಕ್‌ಟಾಪ್ ಸಾಧನದಲ್ಲಿ ಸ್ಥಾಪಿಸಬಹುದು ಮತ್ತು ಬಳಸಬಹುದು.
+ಅನುಸ್ಥಾಪನೆಯ ಸಮಯದಲ್ಲಿ, ಇದು ಹೋಮ್ ಸ್ಕ್ರೀನ್ ಲಾಂಚರ್‌ನಿಂದ ಪ್ರವೇಶಿಸಬಹುದಾದ ಸ್ಥಳೀಯ ಅಪ್ಲಿಕೇಶನ್‌ನಂತೆ ವರ್ತಿಸುತ್ತದೆ. ಯಾವಾಗಲೂ ನವೀಕೃತವಾಗಿರುವಾಗ ಇದು ವೇಗವಾದ ಮತ್ತು ಹೆಚ್ಚು ಅನುಕೂಲಕರ ಅನುಭವವನ್ನು ನೀಡುತ್ತದೆ.
+
+## ಅನುಸ್ಥಾಪನಾ ಸೂಚನೆಗಳು
+### ಆಂಡ್ರಾಯ್ಡ್
+- ವಿಳಾಸ ಪಟ್ಟಿಯ ಪಕ್ಕದಲ್ಲಿರುವ ಮೆನು ಐಕಾನ್ ಮೇಲೆ ಕ್ಲಿಕ್ ಮಾಡಿ
+- "ಮುಖಪುಟ ಪರದೆಗೆ ಸೇರಿಸು" ಆಯ್ಕೆಮಾಡಿ ಮತ್ತು ದೃಢೀಕರಿಸಿ
+
+<img src="${a}" alt="" width="250px" />
+<img src="${n}" alt="" width="250px" />
+
+### ಐಒಎಸ್
+- ಸಫಾರಿಯಲ್ಲಿ ಸೈಟ್ ತೆರೆಯಿರಿ
+- ಕೆಳಭಾಗದಲ್ಲಿರುವ ಹಂಚಿಕೆ ಐಕಾನ್ ಕ್ಲಿಕ್ ಮಾಡಿ
+- "ಹೋಮ್ ಸ್ಕ್ರೀನ್‌ಗೆ ಸೇರಿಸು" ಕ್ಲಿಕ್ ಮಾಡಿ
+
+<img src="${i}" alt="" width="250px" />
+<img src="${o}" alt="" width="250px" />
+<img src="${r}" alt="" width="250px" />
+
+### ಕ್ರೋಮ್
+- ವಿಳಾಸ ಪಟ್ಟಿಯಲ್ಲಿರುವ ಇನ್‌ಸ್ಟಾಲ್ ಐಕಾನ್ ಕ್ಲಿಕ್ ಮಾಡಿ ಮತ್ತು ದೃಢೀಕರಿಸಿ
+
+<img src="${l}" alt="" width="750px" />
+`},b=()=>{const[t,d]=e.useState(s.getItem("lang")||m[0].code);return e.useEffect(()=>{s.setItem("lang",t),document.documentElement.setAttribute("lang",t),g.changeLanguage(t)},[t]),e.createElement(e.Fragment,null,e.createElement(h,{setLang:d}),e.createElement(w,null,c[t]||c.en))},f=p(document.getElementById("root"));f.render(e.createElement(b,null));
+//# sourceMappingURL=install-f4e66a5d.js.map
